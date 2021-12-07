85e35279916c26561255116978c7be0ffb032f67
Felipe <luis.velasquez01@correo.usa.edu.co>
2021-12-06 20:37:50 -0500

    Pruebas de 1 y 2 tipos de gasolina

--- scraping.py
+++ scraping.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import csv
+import pandas as pd
 
 def getDate(data):
     dates = data.find_all('cbc:IssueDate')
@@ -23,11 +25,71 @@ def getSubTotal(data):
     return auxS
 
 
+def getCantidad(data):
+    cantidades = data.find_all('cbc:BaseQuantity')
+    auxC=[]
+    for cantidad in cantidades:
+      iter=float(cantidad.get_text())
+      auxC.append(iter)
+    return auxC
+
+def getPrecioCompra(data):
+    cantidades = data.find_all('cbc:TaxInclusiveAmount')
+    return cantidades
+
+def getValores(data):
+    valores = data.find_all('cbc:TaxAmount')
+    auxI=[]
+    for valor in valores:
+      iter=float(valor.get_text())
+      auxI.append(iter)
+    return list(set(auxI))
+
+def organizarValores(valores,tipos):
+    tamTipos=len(tipos)
+    if tamTipos==1:
+        iva=[valores[3]]
+        sobretasa=[valores[2]]
+        soldicom=[valores[4]]
+    elif tamTipos==2:
+        iva=[valores[3],valores[14]]
+        sobretasa=[valores[2],valores[9]]
+        soldicom=[valores[13],valores[6]]
+    else:
+        pass
+    return iva,sobretasa,soldicom
+
+
+def generateCSV(fecha,proveedor,cantidades,tipos,subtotales,iva,sobretaas,soldicom):
+    fields = ['Fecha', 'Proveedor', 'Cantidad','Tipo','Valor Corriente','Iva','Valor Sobretasa','Soldicom']
+    rows = []
+
+    for i in range(len(tipos)):
+        row = [fecha, proveedor,cantidades[i],tipos[i],subtotales[i],iva[i],sobretaas[i],soldicom[i]]
+        rows.append(row)
+
+    with open('prueba.xlsx', 'w') as f:
+        write = csv.writer(f)
+        write.writerow(fields)
+        write.writerows(rows)
+    eliminarFilasVacias()
+
+def eliminarFilasVacias():
+    df = pd.read_csv('prueba.xlsx')
+    df.to_csv('prueba.xlsx', index=False)
+
+
 if __name__ == "__main__":
-    infile = open("prueba.xml","r")
+    infile = open("prueba1.xml","r")
     contents = infile.read()
     soup = BeautifulSoup(contents,'xml')
     fecha=getDate(soup)
     proveedor='BIOMAX SA'
     tipos=getTypes(soup) 
     subtotales=getSubTotal(soup)
+    cantidades=getCantidad(soup)
+    # precioCompra=getPrecioCompra(soup)
+    valores=getValores(soup)
+    iva,sobretaas,soldicom=organizarValores(valores,tipos)
+    generateCSV(fecha,proveedor,cantidades,tipos,subtotales,iva,sobretaas,soldicom)
+    print(fecha,proveedor,cantidades,tipos,subtotales,iva,sobretaas,soldicom)
